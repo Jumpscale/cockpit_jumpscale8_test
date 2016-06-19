d468ccb9ffa8885acc5d3ffefb5b992e155aeca7
Reem Khamis <khamisr@codescalers.com>
2016-06-19 16:39:34 +0200

    report test results to telegram channel

--- js8tests/servicetemplates/js8testsrunner/actions.py
+++ js8tests/servicetemplates/js8testsrunner/actions.py
@@ -1,12 +1,15 @@
 from JumpScale import j
 
+
 class Actions(ActionsBaseMgmt):
 
-
-
     def install(self, service):
-        e = service.executor
-        e.execute("nosetests /opt/jumpscale8/tests")
- 
+        service.executor.cuisine.pip.install('nose')
+        service.executor.cuisine.core.file_link('$codeDir/github/jumpscale/jumpscale_core8/tests', '$base/tests')
+        rc, tests = service.executor.cuisine.core.run("nosetests $base/tests", die=False)
+        self.report(service, tests)
 
-
+    def report(self, service, message=''):
+        telegram_channel = service.hrd.getStr('telegram.channel', None)
+        self.ask_telegram(channel=telegram_channel, message=message, keyboard=[], expect_response=False, timeout=900, redis=None)
+        return
--- js8tests/servicetemplates/ports_checker/actions.py
+++ js8tests/servicetemplates/ports_checker/actions.py
@@ -1,8 +1,7 @@
 from JumpScale import j
 
+
 class Actions(ActionsBaseMgmt):
-
-
 
     def install(self, service):
         e = service.executor
@@ -11,5 +10,12 @@ class Actions(ActionsBaseMgmt):
             rc, out = e.cuisine.core.run("netstat -nltp | grep {0}".format(p), die=False)
             if rc == 0:
                 j.logger.log("Service is running on port {0}".format(p))
+                self.report(service, "Service is running on port {0}".format(p))
             else:
                 j.logger.log("No service is running on port {0}".format(p))
+                self.report(service, "No service is running on port {0}".format(p))
+
+    def report(self, service, message=''):
+        telegram_channel = service.hrd.getStr('telegram.channel', None)
+        self.ask_telegram(channel=telegram_channel, message=message, keyboard=[], expect_response=False, timeout=900, redis=None)
+        return
