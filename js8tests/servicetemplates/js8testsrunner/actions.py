682d5010fd9c113fb052d50e25e02820e947ac1f
Reem Khamis <khamisr@codescalers.com>
2016-06-20 17:19:29 +0200

    Prettier telegram output

--- js8tests/servicetemplates/js8testsrunner/actions.py
+++ js8tests/servicetemplates/js8testsrunner/actions.py
@@ -11,5 +11,5 @@ class Actions(ActionsBaseMgmt):
 
     def report(self, service, message=''):
         telegram_channel = service.hrd.getStr('telegram.channel', None)
-        self.ask_telegram(channel=telegram_channel, message=message, keyboard=[], expect_response=False, timeout=900, redis=None)
+        self.ask_telegram(channel=telegram_channel, message="""```%s```""" % tests, keyboard=[], expect_response=False, timeout=900, redis=None)
         return
